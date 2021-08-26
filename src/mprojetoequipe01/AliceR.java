7e19792d026e9ece65fe67189106a4679ebbb585
aricaldonee <aricaldone@gmail.com>
2021-08-26 09:32:57 -0300

    Classe aliceR

---
+++ src/mprojetoequipe01/AliceR.java
@@ -0,0 +1,21 @@
+package mprojetoequipe01;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alice
+ */
+public class AliceR {
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Alice Ricaldone");
+    }
+    
+}
